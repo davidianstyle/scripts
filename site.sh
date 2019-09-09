47313da8e5d11cdde5b08ef4a3bb5795e3091265
David Chang <davidianstyle@gmail.com>
2019-09-09 19:59:20 +0000

    Make site.sh more interactive and more robust

--- site.sh
+++ site.sh
@@ -5,8 +5,12 @@ RESTORE=0
 VERBOSE=0
 # OUTPUT='1> /dev/null'
 # ERROR='2>&1'
-while getopts ":brvoe" opt; do
+while getopts "w:brvp:d:f:oe" opt; do
 	case $opt in
+		w)
+			WEBSITE="$OPTARG"
+			# echo "-w was triggered!" >&2
+			;;
 		b)
 			BACKUP=1
 			# echo "-b was triggered!" >&2
@@ -18,6 +22,18 @@ while getopts ":brvoe" opt; do
 		v)
 			VERBOSE=1
 			# echo "-v was triggered!" >&2
+			;;
+		p)
+			DBPASSWORD="$OPTARG"
+			# echo "-p was triggered!" >&2
+			;;
+		d)
+			RESTOREDATE="$OPTARG"
+			# echo "-d was triggered!" >&2
+			;;
+		f)
+			FREQUENCY="$OPTARG"
+			# echo "-f was triggered!" >&2
 			;;
 		# o)
 		# 	OUTPUT=""
@@ -43,14 +59,32 @@ fi
 # Grab the script arguments
 # Make sure that special characters are escaped when used on the command line!
 shift $(($OPTIND - 1))
-WEBSITE=$1
-DBPASSWORD=$2
-RESTOREDATE=$3
 
-if [ $RESTORE -eq 1 ] && [ -z $RESTOREDATE ]
+while [ -z $WEBSITE ]
+do
+	echo "Please specify a website: "
+	read WEBSITE
+done
+
+# Prompt for database password if none is specified
+if [ -z $DBPASSWORD ]
 then
-	read -s "Please specify a date to restore (YYYYMMDD): " RESTOREDATE
+	echo "Please specify a database password: "
+	read -s DBPASSWORD
 fi
+
+# Grab restore date if not provided
+while [ $RESTORE -eq 1 ] && [[ ! "$RESTOREDATE" =~ ^([0-9]{8})$ ]]
+do
+	echo "Please specify a date to restore (YYYYMMDD): "
+	read RESTOREDATE
+done
+
+while [[ ! "$FREQUENCY" =~ ^(daily|monthly|yearly)$ ]]
+do
+	echo "Please specify a valid frequency (daily|monthly|yearly): "
+	read FREQUENCY
+done
 
 # Set variable values
 if [ $BACKUP -eq 1 ]
@@ -61,22 +95,18 @@ else
 fi
 
 YEAR=`date +%Y`
-DIRECTORY="/home/dchang/backup/sites/${WEBSITE}"
-FULLDIRECTORY="${DIRECTORY}/${DATE}"
-DATABASE="${WEBSITE}db"
-SQLFILENAME="${DATABASE}.${DATE}.sql"
-SQLFILE="${FULLDIRECTORY}/${SQLFILENAME}"
-SQLFILEERROR="${FULLDIRECTORY}/${SQLFILENAME}.error"
+RESTOREDIRECTORY="/home/dchang/restores/${WEBSITE}"
+BACKUPDIRECTORY="/home/dchang/backups/${WEBSITE}"
+FULLBACKUPDIRECTORY="${BACKUPDIRECTORY}/${DATE}"
+DATABASE="${WEBSITE}_db"
+SQLFILENAME="${WEBSITE}-${DATE}.sql"
+SQLFILE="${FULLBACKUPDIRECTORY}/${SQLFILENAME}"
+SQLFILEERROR="${FULLBACKUPDIRECTORY}/${SQLFILENAME}.error"
 TARFILENAME="${WEBSITE}-${DATE}.tar.gz"
-TARFILE="${DIRECTORY}/${TARFILENAME}"
+TARFILE="${BACKUPDIRECTORY}/${TARFILENAME}"
 ANNUALTARFILENAME="${WEBSITE}-${YEAR}.tar.gz"
-ANNUALTARFILE="${DIRECTORY}/${ANNUALTARFILENAME}"
-
-# Prompt for database password if none is specified
-if [ -z $DBPASSWORD ]
-then
-	read -s -p "Please enter password for $DATABASE: " DBPASSWORD
-fi
+ANNUALTARFILE="${BACKUPDIRECTORY}/${ANNUALTARFILENAME}"
+S3LOCATION="s3://david-chang-websites/$WEBSITE/$FREQUENCY"
 
 if [ $BACKUP -eq 1 ]
 then
@@ -91,8 +121,8 @@ then
 	fi
 
 	# Create the date directory if it doesn't exist
-	if [ ! -d $FULLDIRECTORY ]; then
-	    mkdir $FULLDIRECTORY
+	if [ ! -d $FULLBACKUPDIRECTORY ]; then
+	    mkdir $FULLBACKUPDIRECTORY
 	fi
 
 	# Perform the rsync
@@ -100,36 +130,28 @@ then
 	then
 		echo "Running rsync..."
 	fi
-	/usr/bin/rsync -avr /home/dchang/sites/$WEBSITE $FULLDIRECTORY 1> /dev/null # 2>&1 $OUTPUT $ERROR
+	/usr/bin/rsync -avr /var/www/$WEBSITE $FULLBACKUPDIRECTORY 1> /dev/null # 2>&1 $OUTPUT $ERROR
 
 	# Do the mysql dump
 	if [ $VERBOSE -eq 1 ]
 	then
 		echo "Running mysqldump..."
 	fi
-	/usr/bin/mysqldump -u dchang -p${DBPASSWORD} $DATABASE > $SQLFILE # 2> $SQLFILEERROR
+	/usr/bin/mysqldump -u dchang -p${DBPASSWORD} $DATABASE > $SQLFILE 2> $SQLFILEERROR
 
 	# Compress everything
 	if [ $VERBOSE -eq 1 ]
 	then
 		echo "Compressing..."
 	fi
-	tar -C $FULLDIRECTORY -zcvf $TARFILE $WEBSITE $SQLFILENAME 1> /dev/null # 2>&1 $OUTPUT $ERROR
-
-	# Create annual archive
-	# if [ $VERBOSE -eq 1 ]
-	# then
-	# 	echo "Creating annual archive..."
-	# fi
-	# tar -C $DIRECTORY -zcvf $ANNUALTARFILE ${YEAR}* 1> /dev/null # 2>&1 $OUTPUT $ERROR
+	tar -C $FULLBACKUPDIRECTORY -zcvf $TARFILE $WEBSITE $SQLFILENAME 1> /dev/null # 2>&1 $OUTPUT $ERROR
 
 	# Send it to S3
 	if [ $VERBOSE -eq 1 ]
 	then
 		echo "Syncing to S3..."
 	fi
-	aws s3 cp $TARFILE s3://$WEBSITE/backup/$TARFILENAME 1> /dev/null # 2>&1 $OUTPUT $ERROR
-	# aws s3 cp $ANNUALTARFILE s3://$WEBSITE/backup/$ANNUALTARFILENAME 1> /dev/null # 2>&1 $OUTPUT $ERROR
+	aws s3 cp $TARFILE $S3LOCATION/$TARFILENAME 1> /dev/null # 2>&1 $OUTPUT $ERROR
 fi
 
 if [ $RESTORE -eq 1 ]
@@ -144,14 +166,14 @@ then
 	then
 		echo "Syncing from S3..."
 	fi
-	aws s3 cp s3://$WEBSITE/backup/$TARFILENAME /home/dchang/$TARFILENAME 1> /dev/null # 2>&1 $OUTPUT $ERROR
+	aws s3 cp $S3LOCATION/$TARFILENAME $RESTOREDIRECTORY/$TARFILENAME 1> /dev/null # 2>&1 $OUTPUT $ERROR
 
 	# Extract from archive
 	if [ $VERBOSE -eq 1 ]
 	then
 		echo "Extracting from archive..."
 	fi
-	tar -zxvf /home/dchang/$TARFILENAME
+	tar -zxvf $RESTOREDIRECTORY/$TARFILENAME -C $RESTOREDIRECTORY
 
 	# MySQL drop database if it exists
 	mysql -u dchang -p${DBPASSWORD} -e "drop database if exists ${DATABASE}"
@@ -164,16 +186,16 @@ then
 	then
 		echo "Running MySQL restore..."
 	fi
-	mysql -u dchang -p${DBPASSWORD} $DATABASE < /home/dchang/$SQLFILENAME
+	mysql -u dchang -p${DBPASSWORD} $DATABASE < $RESTOREDIRECTORY/$SQLFILENAME
 
-	# Create symlink from /var/www directory
-	if [ ! -h /var/www/${WEBSITE} ]
+	# Create document root directory in /var/www
+	if [ ! -d /var/www/${WEBSITE} ]
 	then
 		if [ $VERBOSE -eq 1 ]
 		then
-			echo "Creating symlink..."
+			echo "Creating www directory..."
 		fi
-		ln -s /home/dchang/sites/${WEBSITE}/ /var/www/${WEBSITE}
+		mkdir /var/www/${WEBSITE}
 	fi
 
 	# Create log directory in /etc/httpd/logs
@@ -185,13 +207,6 @@ then
 		fi
 		mkdir /etc/httpd/logs/${WEBSITE}
 	fi
-
-	# Restart apache
-	# /etc/init.d/httpd restart
-
-	# File cleanup
-	# rm /home/dchang/tmp/$SQLFILENAME
-	# rm /home/dchang/tmp/$TARFILENAME
 fi
 
 if [ $VERBOSE -eq 1 ]
